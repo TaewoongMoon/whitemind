65ce786ddb098d748a8292b42930c6d727597ad0
TaewoongMoon <taewoongmoon1@gmail.com>
2021-07-28 21:31:51 +0900

    Fix: delete react hooks and function as they moved from presenter to container page

--- src/components/main/Main.presenter.js
+++ src/components/main/Main.presenter.js
@@ -13,51 +13,49 @@ import {
   ThirdContentWrapper,
   Wrapper,
 } from './Mains.styles'
-import axios from 'axios'
 import InfiniteScroll from 'react-infinite-scroller'
-import { useEffect, useState } from 'react'
 
-const MainUI = () => {
-  const [items, setItems] = useState([])
-  const [totalItems, setTotalitems] = useState([])
-  useEffect(() => {
-    async function getTotal() {
-      await axios
-        .get('https://jsonplaceholder.typicode.com/comments')
-        .then((res) => setTotalitems(res.data))
-        .catch((error) => console.log(error))
-    }
-    getTotal()
-    fetchData(items.length / 10 + 1)
-  }, [])
+const MainUI = (props) => {
+  // const [items, setItems] = useState([])
+  // const [totalItems, setTotalitems] = useState([])
+  // useEffect(() => {
+  //   async function getTotal() {
+  //     await axios
+  //       .get('https://jsonplaceholder.typicode.com/comments')
+  //       .then((res) => setTotalitems(res.data))
+  //       .catch((error) => console.log(error))
+  //   }
+  //   getTotal()
+  //   fetchData(items.length / 10 + 1)
+  // }, [])
 
-  async function fetchData(page) {
-    if (totalItems.length === items.length) return
-    let url = 'https://jsonplaceholder.typicode.com/comments'
-    if (page) {
-      url = `${url}?_page=${page}&_limit=10`
-    }
-    await axios
-      .get(url)
-      .then((res) => setItems([...items, ...res.data]))
-      .catch((err) => console.log(err))
-  }
+  // async function fetchData(page) {
+  //   if (totalItems.length === items.length) return
+  //   let url = 'https://jsonplaceholder.typicode.com/comments'
+  //   if (page) {
+  //     url = `${url}?_page=${page}&_limit=10`
+  //   }
+  //   await axios
+  //     .get(url)
+  //     .then((res) => setItems([...items, ...res.data]))
+  //     .catch((err) => console.log(err))
+  // }
 
   return (
     <Wrapper>
       <InfiniteScroll
         pageStart={1}
-        loadMore={() => fetchData(items.length / 10 + 1)}
+        loadMore={() => props.fetchData(props.items.length / 10 + 1)}
         hasMore={true || false}
         loader={
-          items?.length === totalItems?.length ? null : (
+          props.items?.length === props.totalItems?.length ? null : (
             <div className="loader" key={0}>
               Loading ...
             </div>
           )
         }
       >
-        {items?.map((data, index) => (
+        {props.items?.map((data, index) => (
           <CardContainer key={index}>
             <ContentWrapper>
               <FirstContentWrapper>
